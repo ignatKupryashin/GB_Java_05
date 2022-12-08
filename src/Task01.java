acd345eb80304c46c3c0a6874ca297df8e30ab1a
ignatKupryashin <ikupryashin@gmail.com>
2022-12-08 22:26:48 +0300

    Сделал первую задачу

--- src/Task01.java
+++ src/Task01.java
@@ -1,6 +1,7 @@
 //Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
 
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -13,7 +14,7 @@ public class Task01 {
         addNote(telBook, "Иванов Иван", "+71111111111" );
         addNote(telBook, "Фёдоров Фёдор", "+72222222222" );
         addTel(telBook, "Иванов Иван", "+73333333333");
-//        System.out.println(telBook);
+        System.out.println(telBook);
         
     }
 
@@ -21,15 +22,16 @@ public class Task01 {
         if (!telBook.containsKey(name)) {
             ArrayList<String> tempArray = new ArrayList<>(2);
             tempArray.add(tel);
-            telBook.put(name,telBook);
+            telBook.put(name,tempArray);
             System.out.println(telBook);
         }
     }
 
     public static void addTel(HashMap <String, ArrayList<String>> telBook, String name, String tel) {
         if (telBook.containsKey(name)) {
-            System.out.println( telBook.get(name).toString());
-
+            ArrayList<String> tempArray = telBook.get(name);
+            tempArray.add(tel);
+            telBook.put(name, tempArray);
         }
     }
 }
--- src/Task04.java
+++ src/Task04.java
@@ -1,4 +1,23 @@
-//На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
-
-public class Task04 {
-}
+////На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//public class Task04 {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public static void printDeck(List<List<Integer>> units) {
+//        for (int i = 0; i < 8; i++) {
+//            System.out.print("____________________");
+//            for (int j = 0; j < 8; j++) {
+//                System.out.print("|");
+//                if (units.)
+//
+//            }
+//
+//        }
+//    }
+//}
